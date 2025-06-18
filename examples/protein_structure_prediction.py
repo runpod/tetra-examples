de2f50b75e97d75df3f9a15cb682af05a92b534b
Dean Quiñanola <dean.quinanola@runpod.io>
2025-06-17 22:15:02 -0700

    fix: no longer need `load_dotenv()` per run
    This is implicit in Tetra library now

--- examples/protein_structure_prediction.py
+++ examples/protein_structure_prediction.py
@@ -1,9 +1,6 @@
 import asyncio
-from dotenv import load_dotenv
 from tetra_rp import remote, LiveServerless, GpuGroup
 
-# Load environment variables from .env file
-load_dotenv()
 
 # Configuration for a GPU resource
 gpu_config = LiveServerless(
